c9b87e95edc8334d417f9e52477960909446ba8c
taosang1992 <taosang1992@gmail.com>
2017-12-14 21:48:50 +0800

    update homepage

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 var app = new Vue({
   el: '#app',
   data:{
-    msg: 'Hello world'
+    works_data:[]
   },
   methods:{
 
   },
   created: function () {
-    axios.get('')
+    var self = this;
+    axios.get('https://taosang1992.github.io/works/data_json/works.json')
       .then(function (response) {
-        console.log(response);
+        // console.log(response.data);
+        self.works_data = response.data;
       })
       .catch(function (error) {
         console.log(error);
