f7a41720f97544da5f6a7557f5b63f2fa47001a9
taosang1992 <taosang1992@gmail.com>
2017-12-15 08:38:44 +0800

    update works.json

--- main.js
+++ main.js
@@ -4,17 +4,26 @@ var app = new Vue({
     works_data:[]
   },
   methods:{
-
+    getComponents: function () {
+      var self = this;
+      axios.get('https://taosang1992.github.io/works/data_json/works.json')
+        .then(function (response) {
+          // console.log(response.data);
+          self.works_data = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    getProjects: function () {
+     var self = this;
+     self.works_data = self.works_data.filter(function (item) {
+       return item.type = "project";
+     })
+    }
   },
   created: function () {
     var self = this;
-    axios.get('https://taosang1992.github.io/works/data_json/works.json')
-      .then(function (response) {
-        // console.log(response.data);
-        self.works_data = response.data;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    self.getComponents();
   }
 });
